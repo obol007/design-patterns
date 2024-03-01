64082cce55a271f835a64c1a96998a9015fc73f8
Piotr Obolewicz <p.obolewicz@il-pib.pl>
2024-03-01 18:35:21 +0100

    1 commit

---
+++ src/test/java/pl/commitow/piotr/obolewicz/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.commitow.piotr.obolewicz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
